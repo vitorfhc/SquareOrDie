62043a8831ac80eb093142f7f3abc1abf3ab3f60
Vitor Falcao <vitorfhcosta@gmail.com>
2017-05-01 14:32:10 -0300

    Cant receive null object as owner

--- src/Engine/Component.cpp
+++ src/Engine/Component.cpp
@@ -1,7 +1,10 @@
 #include "Engine/Component.h"
 #include "Engine/GameObject.h"
+#include "Log/log.h"
 
 Component::Component(GameObject *owner, ComponentType type) {
+  if (!owner)
+    ERROR("Invalid null owner");
   m_owner = owner;
   m_type = type;
   m_owner->AddComponent(this);
